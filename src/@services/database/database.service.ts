5300acd87a6e9342dc858a36cfc1fa87c5e23ff1
Orel Naranjo <or.naranjo@duocuc.cl>
2024-06-04 17:09:56 -0400

    Update Prettier configuration and package dependencies

---
+++ src/@services/database/database.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+@Injectable()
+export class DatabaseService implements TypeOrmOptionsFactory {
+  async createTypeOrmOptions(): Promise<TypeOrmModuleOptions> {
+    return {
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: ['dist/entities/*.entity{.ts,.js}'],
+      migrations: ['dist/db/migrations/*{.ts,.js}'],
+      dropSchema: process.env.DB_DROP_SCHEMA === 'true' || false,
+      synchronize: process.env.DB_SYNCRONIZE === 'true' || false,
+      migrationsTableName: 'migrations',
+      logging: process.env.DB_LOGGING === 'true' || false,
+    };
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Configuración de prefijo global
+  app.setGlobalPrefix('api/v1');
+
+  // Implementación de Swagger
+  const config = new DocumentBuilder()
+    .setTitle('La VeguitaClick API')
+    .setDescription('API para la empresa La VeguitaClick')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('support/api', app, document, {
+    customSiteTitle: 'La VeguitaClick API',
+    customCss: '.swagger-ui .topbar { display: none }',
+  });
+
+  // Implementación de CORS
+  app.enableCors({ origin: 'http://localhost:4200' });
+
+  // Implementación del servidor
+  await app.listen(3000, () => {
+    Logger.log(`Servidor corriendo en http://localhost:3000/api/v1`);
+  });
 }
 bootstrap();
