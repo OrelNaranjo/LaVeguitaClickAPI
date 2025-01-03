5a6137baae3d93674e80d61c9cbf3304dfa7d733
Orel Naranjo <or.naranjo@duocuc.cl>
2025-01-03 18:21:57 -0300

    chore: Refactor tests and update mail service dependencies

--- src/core/decorators/app.decorator.spec.ts
+++ src/core/decorators/app.decorator.spec.ts
@@ -2,16 +2,16 @@ import { SetMetadata } from '@nestjs/common';
 import { GetPrivileges } from './app.decorator';
 
 jest.mock('@nestjs/common', () => ({
-    ...jest.requireActual('@nestjs/common'),
-    SetMetadata: jest.fn(),
+  ...jest.requireActual('@nestjs/common'),
+  SetMetadata: jest.fn(),
 }));
 
 describe('App Decorators', () => {
-    describe('GetPrivileges', () => {
-        it('should call SetMetadata with the correct arguments', () => {
-            GetPrivileges('privilege1', 'privilege2');
+  describe('GetPrivileges', () => {
+    it('should call SetMetadata with the correct arguments', () => {
+      GetPrivileges('privilege1', 'privilege2');
 
-            expect(SetMetadata).toHaveBeenCalledWith('privileges', ['privilege1', 'privilege2']);
-        });
+      expect(SetMetadata).toHaveBeenCalledWith('privileges', ['privilege1', 'privilege2']);
     });
+  });
 });
--- src/shared/services/mails/mails.service.ts
+++ src/shared/services/mails/mails.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import handlebars from 'handlebars';
-import * as hbs from 'nodemailer-express-handlebars';
+import * as handlebars from 'handlebars';
 import * as nodemailer from 'nodemailer';
 import * as dotenv from 'dotenv';
+import hbs from 'nodemailer-express-handlebars';
 
 dotenv.config();
+
 @Injectable()
 export class MailsService {
-  private transporter: nodemailer.Transporter;
-  private companyData: any;
+  private readonly transporter: nodemailer.Transporter;
+  private readonly companyData: any;
 
   constructor() {
     this.transporter = nodemailer.createTransport({
@@ -35,19 +36,18 @@ export class MailsService {
       companyEmail: process.env.COMPANY_EMAIL,
     };
 
-    this.transporter.use(
-      'compile',
-      hbs({
-        viewEngine: {
-          extname: '.hbs',
-          partialsDir: 'src/core/templates/partials/',
-          layoutsDir: 'src/core/templates/layouts/',
-          defaultLayout: 'main',
-        },
-        viewPath: 'src/core/templates/mails/',
-        extName: '.hbs',
-      }),
-    );
+    const hbsOptions = {
+      viewEngine: {
+        extname: '.hbs',
+        partialsDir: 'src/core/templates/partials/',
+        layoutsDir: 'src/core/templates/layouts/',
+        defaultLayout: 'main',
+      },
+      viewPath: 'src/core/templates/mails/',
+      extName: '.hbs',
+    };
+
+    this.transporter.use('compile', hbs(hbsOptions));
   }
 
   public async send(templateName: string, subject: string, data: any): Promise<void> {
